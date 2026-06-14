d1fc2b306af515e66edf9d546367c2a967596608
Sobih <sobihmohamedsobih@gmail.com>
2026-06-14 20:54:59 +0300

    end the implementation of contract

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -2,9 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Context;
-using System;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace Presentation
 {
@@ -29,18 +26,32 @@ namespace Presentation
             Console.WriteLine("======================================");
             Console.WriteLine("    Book Store Initialized    ");
             Console.WriteLine("======================================\n");
-
             using (var scope = serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<BookDbContext>();
 
                 Console.WriteLine("Checking database connection...");
                 if (await dbContext.Database.CanConnectAsync())
+                {
                     Console.WriteLine("Success: Connected to SQL Server successfully!");
-                else
-                    Console.WriteLine("Warning: Cannot connect to database yet (Database might not exist, which is normal before running migrations).");
-            }
 
+                    // Seeding
+                    Console.WriteLine("Applying Initial Data Seed...");
+                    try
+                    {
+                        await Persistence.DataSeed.BookDbContextSeed.SeedAsync(dbContext);
+                        Console.WriteLine("Seeding Completed Successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred during seeding: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Warning: Cannot connect to database yet (Database might not exist, which is normal before running migrations).");
+                }
+            }
             Console.ReadLine();
         }
     }
