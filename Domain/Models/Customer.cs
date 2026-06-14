f7b2ac75c1922816dc0cf898b57144f39b1dc6c3
Sobih <sobihmohamedsobih@gmail.com>
2026-06-14 18:59:25 +0300

    IMPLEMENT THE MDELS AND ALL NTERFACES OF THE DESIGN PATTERNS & EVALUATOR

---
+++ Domain/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models
+{
+    public class Customer : BaseEntity<int>
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
